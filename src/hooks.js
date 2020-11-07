f0918f4e4e4ae6bd5b38e5e39581e874d223a031
Anwesh43 <anweshthecool0@gmail.com>
2020-11-08 04:07:50 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,12 @@ import {
     useState, 
     useEffect
 } from 'react'
+import {
+    divideScale, 
+    sinify
+} from './utils'
 
+const parts = 3
 export const useAniamtedScale = (scGap = 0.02, delay = 20) => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
@@ -13,7 +18,7 @@ export const useAniamtedScale = (scGap = 0.02, delay = 20) => {
                 setAnimated(true)
                 let currScale = 0 
                 const interval = setInterval(() => {
-                    currScale += scGap 
+                    currScale += (scGap / parts)
                     setScale(currScale)
                     if (currScale > 1) {
                         setAnimated(false)
@@ -36,12 +41,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {  
         w, 
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const sf = sinify(scale)
+    const size = Math.min(w, h) / 10 
+    const position = 'absolute'
+    const background = 'green'
+    const strokeWidth = Math.min(w, h) / 90 
+    const gap = size / 2 
+    return {
+        parentStyle() {
+            const left = `${w / 2}px`
+            const top = `${h}px`
+            return {
+                position, 
+                left,
+                top
+            }
+        },
+        getLineStyle(i) {
+            const sf1 = divideScale(sf, 0, parts)
+            const left = `${-gap / 2 + gap * i - strokeWidth / 2}px`
+            const top = `${-size * sf1}px`
+            const width = `${strokeWidth}px`
+            const height = `${size * sf1}px`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height,
+                background
+            }
+        },
+        getFillStyle() {
+            const sf2 = divideScale(sf, 1, parts)
+            const left = `${-gap / 2}px`
+            const top = `${-size * sf2}px`
+            const width = `${gap}px`
+            const height = `${size * sf2}px`
+            return {
+                position,
+                top, 
+                left, 
+                width, 
+                height, 
+                background
+            }
+        }
     }
 }
